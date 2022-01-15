27472ae291fc603b0e89d2ef55a412cfd3b25437
ntduyen <duyenbeofcbb@gmail.com>
2022-01-16 00:14:15 +0700

    signin 1

---
+++ admin/signin.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Netflix signin page</title>
+    <link rel="stylesheet" href="CSS/signin.css">
+</head>
+<body>
+<div class="wrapper">
+        <div class="header">
+            <div class="logo">
+                <a href="#"><img src="./CSS/image/netflixlogo.png" alt="logo" srcset=""></a>
+            </div>
+        </div>
+        <div class="login__body">
+            <div class="login__box">
+                <h2>Sign in</h2>
+                <form>
+                    <div class="input__wrap">
+                        <input type="text" placeholder="Email or phone number">
+                    </div>
+                    <div class="input__wrap">
+                        <input type="text" placeholder="Password">
+                    </div>
+                    <div class="input__wrap">
+                        <button>Sign in</button>
+                    </div>
+                    <div class="support">
+                        <div class="remember">
+                            <span> <input type="checkbox"></span>
+                            <span>Remember me</span>
+                        </div>
+                        <div class="need__help">
+                            Need help ?
+                        </div>
+                      
+                    </div>
+                    <div class="login__footer">
+                        <div class="login__facebook">
+                            <span><img src="./css/image/fb icon.png" alt="fb"></span>
+                            <span><a href="#">Login with Facebook</a></span>
+                        </div>
+                        <div class="sign__up">
+                            <p>New to Netflix? <a href="#">sign up now.</a></p>
+                        </div>
+                        <div class="terms">
+                            <p>This page is protected by Google reCAPTCHA to make sure you're not a robot <a href="#">Lear more.</a></p>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+
+</div>
+
+
+</body>
+</html>

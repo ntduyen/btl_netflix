ff04a7967df8928295abbf69c243667d9d6703c8
ntduyen <duyenbeofcbb@gmail.com>
2022-01-16 00:54:24 +0700

    sigin 2

--- signin.php
+++ signin.php
@@ -4,14 +4,14 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Netflix signin page</title>
+    <title>Netflix login page</title>
     <link rel="stylesheet" href="CSS/signin.css">
 </head>
 <body>
-<div class="wrapper">
+    <div class="wrapper">
         <div class="header">
             <div class="logo">
-                <a href="#"><img src="./CSS/image/netflixlogo.png" alt="logo" srcset=""></a>
+                <a href="#"><img src="Image/netflixlogo.png" alt="logo" srcset=""></a>
             </div>
         </div>
         <div class="login__body">
@@ -19,7 +19,7 @@
                 <h2>Sign in</h2>
                 <form>
                     <div class="input__wrap">
-                        <input type="text" placeholder="Email or phone number">
+                        <input type="text" placeholder="Email or username">
                     </div>
                     <div class="input__wrap">
                         <input type="text" placeholder="Password">
@@ -39,22 +39,20 @@
                     </div>
                     <div class="login__footer">
                         <div class="login__facebook">
-                            <span><img src="./css/image/fb icon.png" alt="fb"></span>
+                            <span><img src="Image/fb icon.png" alt="fb"></span>
                             <span><a href="#">Login with Facebook</a></span>
                         </div>
                         <div class="sign__up">
-                            <p>New to Netflix? <a href="#">sign up now.</a></p>
+                            <p>New to Netflix? <a href="#">Sign up now.</a></p>
                         </div>
                         <div class="terms">
-                            <p>This page is protected by Google reCAPTCHA to make sure you're not a robot <a href="#">Lear more.</a></p>
+                            <p>This page is protected by Google reCAPTCHA to make sure you're not a robot <a href="#">Learn more.</a></p>
                         </div>
                     </div>
                 </form>
             </div>
         </div>
-
-</div>
-
-
+   
+    </div>
 </body>
 </html>
